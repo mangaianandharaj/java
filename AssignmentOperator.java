8552e4ef447048db51988c3606a94a6f37dec78b
mangaianandharaj <132830602+mangaianandharaj@users.noreply.github.com>
2024-11-15 13:23:31 +0530

    Add files via upload

---
+++ AssignmentOperator.java
@@ -0,0 +1,28 @@
+package AllProgram;
+
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        int num = 10;
+        String name = "Java";
+
+        System.out.println("num: " + num);
+        System.out.println("name: " + name);
+
+        // += operator
+        int a = 10;
+        a += 10; // a = a + 10
+        System.out.println("Value of a: " + a);
+
+        // && (and) operator > - greater
+        System.out.println((5 > 3) && (8 > 5));
+
+        // && (and) operator < - lesser
+        System.out.println((5 < 3) && (8 < 5));
+
+        // || (or) operator > - greater
+        System.out.println((5 < 3) || (8 < 5));
+
+        // ! (not) operator
+        System.out.println(!(5 < 3));
+    }
+}
