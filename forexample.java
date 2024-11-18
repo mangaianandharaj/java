4ba32b7effea56114956a95f7eb92cb0d91a354d
Mangaiyarkkarasi A <132830602+mangaianandharaj@users.noreply.github.com>
2024-11-18 18:44:06 +0530

    Add files via upload

---
+++ forexample.java
@@ -0,0 +1,13 @@
+package AllProgram;
+
+public class forexample {
+	public static void main(String[] args) {
+	//loop through a block of code
+	//it helps to execute a set of code repeatedly
+	//simple for loop //enchanced for loop //labelled for loop 
+	//for(initialisation;condition;increment/decrement)
+		for(int  i =0;i<=10;i++) {
+			System.out.println(i);
+		}
+	}
+}
