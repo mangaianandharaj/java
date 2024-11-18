e24984ade4d7179affc32f21d9a0e5920d155186
Mangaiyarkkarasi A <132830602+mangaianandharaj@users.noreply.github.com>
2024-11-18 18:43:20 +0530

    Add files via upload

---
+++ contionalstatement.java
@@ -0,0 +1,37 @@
+package AllProgram;
+
+public class contionalstatement {
+// types --. if --.if else --. if else if 
+//based on  certain conditions and generate conditions accordingly
+	public static void main(String[] args) {
+		int age=20;
+		//if(condition)
+		//statements
+		if(age>18) {
+			System.out.println(age+" voting is permitted");
+		}
+		if(age<18) {
+			System.out.println("voting is not permitted");
+		}
+		else {
+			System.out.println("sry, give the valid age");
+		}
+	}
+	
+	
+
+//if statement is executed only on true condition will be done in the current block
+	
+//if else is a control statement that is used in a specific block of a program if the condition is true or false 
+	
+//true will execute the if part and false will execute the else block
+	
+//nested if is nothing but several branches with another branches allows to test multiple criteria and increases the number of possible outcomes
+	
+//if else if is used to evaluate multiple expressions and execute different statement
+}
+
+
+//greatest of three numbers 
+
+
