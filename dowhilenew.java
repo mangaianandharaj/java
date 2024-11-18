be47683e1471173fcebed3240996f19b90255eda
Mangaiyarkkarasi A <132830602+mangaianandharaj@users.noreply.github.com>
2024-11-18 19:18:55 +0530

    Add files via upload

---
+++ dowhilenew.java
@@ -0,0 +1,12 @@
+package AllProgram;
+
+public class dowhilenew {
+	public static void main(String[] args) {
+		//it will return true as long as the condition is true
+		int i =0;
+		do {
+			System.out.println(i);
+			i++;
+		}while(i<5);//end of do while
+	}
+}
