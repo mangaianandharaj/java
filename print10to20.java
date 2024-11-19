e549d15709235d88f424a620760244c8e002af6e
Mangaiyarkkarasi A <132830602+mangaianandharaj@users.noreply.github.com>
2024-11-19 16:26:12 +0530

    Add files via upload

---
+++ print10to20.java
@@ -0,0 +1,13 @@
+package AllProgram;
+
+public class print10to20 {
+
+	    public static void main(String[] args) {
+	        int i = 10;
+	        while (i <= 100) {
+	            System.out.print(i + " ");
+	            i += 10;
+	        }
+	    }
+
+}
