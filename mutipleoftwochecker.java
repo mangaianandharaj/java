2e801bcb6733b4c9547f602730ee3e170b50793c
Mangaiyarkkarasi A <132830602+mangaianandharaj@users.noreply.github.com>
2024-11-20 16:18:57 +0530

    Add files via upload

---
+++ mutipleoftwochecker.java
@@ -0,0 +1,17 @@
+package AllProgram;
+import java.util.Scanner;
+public class mutipleoftwochecker {
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.print("Enter a number: ");
+	        int number = scanner.nextInt();
+
+	        if (number % 2 == 0) {
+	            System.out.println(number + " is a multiple of 2.");
+	        } else {
+	            System.out.println(number + " is not a multiple of 2.");
+	        }
+	    }
+	
+
+}
