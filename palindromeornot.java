a869d94c3956d39d2de68c010806377bfea7d90e
Mangaiyarkkarasi A <132830602+mangaianandharaj@users.noreply.github.com>
2024-11-20 16:17:48 +0530

    Add files via upload

---
+++ palindromeornot.java
@@ -0,0 +1,21 @@
+package AllProgram;
+import java.util.Scanner;
+public class palindromeornot {
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.print("Enter a string: ");
+	        String input = scanner.nextLine();
+	        String reversed = "";
+	        
+	        for (int i = input.length() - 1; i >= 0; i--) {
+	            reversed = reversed + input.charAt(i);
+	        }
+	        
+	        if (input.equals(reversed)) {
+	            System.out.println("Palindrome");
+	        } else {
+	            System.out.println("Not a palindrome");
+	        }
+	    }
+
+}
